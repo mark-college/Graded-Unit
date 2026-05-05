666fd4992fbff0d1f22db24df30407de77e2500a
KrisKiwii <KrisGillies0505@gmail.com>
2026-05-05 11:32:26 +0100

    Enemy (work in progress
    only done animations for now

---
+++ Enemy Code/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+SerializeField private Rigidbody2D rigidbody;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
