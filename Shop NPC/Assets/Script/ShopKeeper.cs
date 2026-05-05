17e0f00761a303ee1463262a93f3616b4b2b1ecc
cici <132680319+zqmls@users.noreply.github.com>
2026-05-05 12:05:22 +0100

    Shop NPC

---
+++ Shop NPC/Assets/Script/ShopKeeper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+    [Header("Shop")]
+    public string shopName = "Armoury";
+    public float interactionRange = 3.5f;
+
+    [Header("Economy")]
+    public int playerGold = 100;
+
+        
+
+}
